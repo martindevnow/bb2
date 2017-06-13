12e583acd7bf470c78bce85386b91e1aacbacff2
Ben Martin <bmartin@univeris.com>
2017-06-12 20:19:33 -0400

    WIP

--- resources/views/layouts/smartadmin/app.blade.php
+++ resources/views/layouts/smartadmin/app.blade.php
@@ -7,6 +7,8 @@
     <title> B.A.R.F.Admin </title>
     <meta name="description" content="">
     <meta name="author" content="">
+    
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 
