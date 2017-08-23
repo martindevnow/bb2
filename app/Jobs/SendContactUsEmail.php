829f3dc87079a9e22c7845e0753990e6813c12eb
Ben Martin <ben@martindevnow.com>
2017-08-23 10:01:44 -0400

    Added jobs

---
+++ app/Jobs/SendContactUsEmail.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\ContactReceived;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Support\Facades\Mail;
+
+class SendContactUsEmail implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle($contactUsData)
+    {
+        Mail::to('info@barfbento.com')
+            ->cc('benm@barfbento.com')
+            ->send(new ContactReceived($contactUsData));
+
+    }
+}
