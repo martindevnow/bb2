743565360cea334d276d13d8709e1ed691a8e72b
Ben Martin <ben@martindevnow.com>
2017-08-23 10:55:13 -0400

    Added command to boot up queue:listener

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,8 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        \App\Console\Commands\GenerateOrders::class
+        \App\Console\Commands\GenerateOrders::class,
+        \App\Console\Commands\EnsureQueueListenerIsRunning::class
     ];
 
     /**
@@ -24,6 +25,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('queue:checkup')->everyFiveMinutes();
         // $schedule->command('inspire')
         //          ->hourly();
     }
