3f18e0f3d8d025a19d79c893b0d9b424ad255f73
Ben Martin <ben@martindevnow.com>
2017-10-23 08:57:23 -0400

    Meat class and loader

--- resources/assets/js/models/Meat.js
+++ resources/assets/js/models/Meat.js
@@ -1,4 +1,4 @@
-class Meat {
+export class Meat {
     constructor(apiData) {
         this.code = apiData.code;
         this.type = apiData.type;
@@ -7,3 +7,15 @@ class Meat {
         this.has_bone = apiData.has_bone;
     }
 }
+
+export const loadMeatFromData = function(data) {
+    let meat = new Meat();
+
+    meat.code = data.code;
+    meat.type = data.type;
+    meat.variety = data.variety;
+    meat.cost_per_lb = data.cost_per_lb;
+    meat.has_bone = data.has_bone;
+
+    return meat;
+};
