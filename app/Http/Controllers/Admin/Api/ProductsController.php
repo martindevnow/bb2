fdaa72eaf3edd7eb96b3d8b9980681751beb2b7d
Ben Martin <ben@martindevnow.com>
2017-11-18 08:56:13 -0500

    Fixed bug where cancelled orders were being exported as pasking slips.

--- app/Http/Controllers/Admin/Api/ProductsController.php
+++ app/Http/Controllers/Admin/Api/ProductsController.php
@@ -22,13 +22,13 @@ class ProductsController extends Controller {
      */
     public function store(Request $request) {
         $validData = $request->validate([
-            'name'          => 'required',
-            'description'   => 'required',
-            'description_long' => 'nullable',
-            'size'          => 'required',
-            'sku'           => 'required',
-            'ingredients'   => 'required',
-            'price'         => 'required|numeric',
+            'name'              => 'required',
+            'description'       => 'required',
+            'description_long'  => 'nullable',
+            'size'              => 'required',
+            'sku'               => 'required',
+            'ingredients'       => 'required',
+            'price'             => 'required|numeric',
         ]);
 
         $product = Product::create($validData);
@@ -43,13 +43,13 @@ class ProductsController extends Controller {
      */
     public function update(Product $product, Request $request) {
         $validData = $request->validate([
-            'name'          => 'required',
-            'description'   => 'required',
-            'description_long' => 'nullable',
-            'size'          => 'required',
-            'sku'           => 'required',
-            'ingredients'   => 'required',
-            'price'         => 'required|numeric',
+            'name'              => 'required',
+            'description'       => 'required',
+            'description_long'  => 'nullable',
+            'size'              => 'required',
+            'sku'               => 'required',
+            'ingredients'       => 'required',
+            'price'             => 'required|numeric',
         ]);
 
         $product->update($validData);
