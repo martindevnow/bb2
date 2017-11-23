f0e70380f423f509ab4023c1b3253baf5ea84d40
Ben Martin <ben@martindevnow.com>
2017-11-22 22:22:58 -0500

    WIP

--- resources/assets/js/vuex/modules/couriers/mutations.js
+++ resources/assets/js/vuex/modules/couriers/mutations.js
@@ -1,3 +1,7 @@
-export const populateCouriersCollection = (state, data) => {
-    state.collection = data;
+import * as mutations from './mutationTypes';
+
+export default {
+    [mutations.POPULATE_COLLECTION] (state, data) {
+        state.collection = data.map(modelData => loadMealFromData(modelData));
+    },
 };
